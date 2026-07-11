b9227e683e26716ecd5eb7050a07f6b0105f88d8
Ahmed Talaat <ahmed.talaat.ali05@gmail.com>
2026-07-11 11:46:34 +0300

    feat: add website icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,9 +79,9 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
+    icon: "/icon.png",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
   },
 
   category: "Productivity",
